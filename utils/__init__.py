fd820014b1f9553ff0aba1af79ef49bf8c91981e
LessGoh <95470108+LessGoh@users.noreply.github.com>
2025-06-05 19:47:38 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,25 +1,2 @@
-"""
-Utils package for Research Q&A Bot
-"""
-
-from .config import Config, config
-from .logger import setup_logger, get_logger
-from .helpers import (
-    ensure_directory_exists,
-    format_response_time,
-    truncate_text,
-    extract_keywords,
-    validate_file_path
-)
-
-__all__ = [
-    "Config",
-    "config",
-    "setup_logger", 
-    "get_logger",
-    "ensure_directory_exists",
-    "format_response_time",
-    "truncate_text",
-    "extract_keywords",
-    "validate_file_path"
-]
+# Minimal utils package
+pass
